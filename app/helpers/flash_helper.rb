a43484ab2318d81b39c9ded6571e6ad6c6a287c8
Raymond Usbal <rsusbal@philippinedev.com>
2016-03-08 01:56:46 +0800

    commented unused code

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -12,21 +12,20 @@ module FlashHelper
   end
 
   def flash_path
-    # File.join(controller_name, 'flash', flash[:from].to_s + '_' + flash[:type].to_s)
     [controller_name, 'flash'].join('/')
   end
 
-  def render_flash_file
-    begin
-      render flash_path, object: flash_object
-    rescue Exception => e
-      # if e.class.name == 'ActionView::MissingTemplate'
-        # render html: '<h4>Errors</h4>'.html_safe
-      # else
-        raise e
-      # end
-    end
-  end
+  # def render_flash_file
+  #   begin
+  #     render flash_path, object: flash_object
+  #   rescue Exception => e
+  #     if e.class.name == 'ActionView::MissingTemplate'
+  #       render html: '<h4>Errors</h4>'.html_safe
+  #     else
+  #       raise e
+  #     end
+  #   end
+  # end
 
   def flash_messages
     flash[:full_messages]
