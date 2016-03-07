489f239211653185e208e6c7e4ea026f4912092b
Raymond Usbal <rsusbal@philippinedev.com>
2016-03-08 02:42:44 +0800

    author: create, update, delete

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,5 @@
 class AuthorsController < ApplicationController
+  before_action :find_id, only: [:edit, :update, :destroy]
 
   def index
     @authors = Author.all
@@ -13,15 +14,40 @@ class AuthorsController < ApplicationController
     @author = Author.new(author_params)
 
     if @author.save
-      flash[:notice] = 'New author saved'
+      set_flash :success, object: @author
       redirect_to authors_path
     else
-      flash[:alert] = 'Not ready to process, please check...'
-      render 'new'
+      set_flash :error, object: @author
+      render :new
+    end
+  end
+
+  def edit
+    render :new
+  end
+
+  def update
+    if @author.update(author_params)
+      set_flash :success, object: @author
+      redirect_to authors_path
+    else
+      set_flash :error, object: @author
+      render :new
+    end
+  end
+
+  def destroy
+    if @author.destroy
+      flash[:success] = @author.first_name + ' ' + @author.last_name + ' was deleted.'
+      redirect_to authors_path
     end
   end
 
   private
+
+  def find_id
+    @author = Author.find(params[:id])
+  end
 
   def author_params
     params.require(:author).permit(:first_name, :last_name)
